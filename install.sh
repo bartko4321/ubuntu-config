08a9bd1ee1d5fc2a7278de8e8e3f657e379a495e
Bartosz Szczeciński <54596452+bartko4321@users.noreply.github.com>
2026-08-17 12:19:53 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -260,6 +260,13 @@ safe_apt_update
 sudo apt-get upgrade -yq || true
 sudo apt-get autoremove -yq || true
 
+rm -rf ~/.local/share/akonadi ~/.local/share/kmail2 ~/.local/share/local-mail ~/.local/share/contacts ~/.local/share/korganizer ~/.local/share/akregator ~/.local/share/kontact ~/.local/share/konqueror
+rm -rf ~/.config/akonadi* ~/.config/kmail* ~/.config/kontact* ~/.config/korganizer* ~/.config/kaddressbook* ~/.config/akregator* ~/.config/emailidentities ~/.config/mailtransports
+rm -rf ~/.cache/akonadi* ~/.cache/kmail* ~/.cache/kontact* ~/.cache/korganizer* ~/.cache/kaddressbook* ~/.cache/akregator* ~/.cache/konqueror*
+rm -rf ~/.local/share/{epiphany,decibels,gnome-user-docs,gnome-contacts,gnome-maps,gnome-weather}
+rm -rf ~/.config/{epiphany,decibels,gnome-user-docs,gnome-contacts,gnome-maps,gnome-weather}
+rm -rf ~/.cache/{epiphany,decibels,gnome-user-docs,gnome-contacts,gnome-maps,gnome-weather}
+
 # ==========================================================
 # ETAP 2/3: INSTALACJA PAKIETÓW I FLATPAK
 # ==========================================================
